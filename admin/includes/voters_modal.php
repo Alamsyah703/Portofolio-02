a0919196eeeb5e2febee8ecc7f82e1d8b001fdd0
Alamsyah703 <21082010249@student.upnjatim.ac.id>
2023-06-24 12:43:13 +0700

    commit

---
+++ admin/includes/voters_modal.php
@@ -0,0 +1,197 @@
+<!-- Add -->
+<div class="modal fade" id="addnew">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title"><b>Add New Voter</b></h4>
+            </div>
+            <div class="modal-body">
+                <form class="form-horizontal" method="POST" action="voters_add.php" enctype="multipart/form-data">
+                <div class="form-group">
+  <label for="voters_id" class="col-sm-3 control-label">Voters ID</label>
+  <div class="col-sm-9">
+    <input type="text" class="form-control" id="voters_id_input" name="voters_id" readonly>
+  </div>
+</div>
+<!-- Rest of the form fields -->
+
+                    <div class="form-group">
+                        <label for="firstname" class="col-sm-3 control-label">Firstname</label>
+                        <div class="col-sm-9">
+                            <input type="text" class="form-control" id="firstname" name="firstname" required>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="lastname" class="col-sm-3 control-label">Lastname</label>
+                        <div class="col-sm-9">
+                            <input type="text" class="form-control" id="lastname" name="lastname" required>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="password" class="col-sm-3 control-label">Password</label>
+                        <div class="col-sm-9">
+                            <input type="password" class="form-control" id="password" name="password" required>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="photo" class="col-sm-3 control-label">Photo</label>
+                        <div class="col-sm-9">
+                            <input type="file" id="photo" name="photo">
+                        </div>
+                    </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal">
+                    <i class="fa fa-close"></i> Close
+                </button>
+                <button type="submit" class="btn btn-primary btn-flat" name="add">
+                    <i class="fa fa-save"></i> Save
+                </button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Edit -->
+<div class="modal fade" id="edit">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span></button>
+              <h4 class="modal-title"><b>Edit Voter</b></h4>
+            </div>
+            <div class="modal-body">
+              <form class="form-horizontal" method="POST" action="voters_edit.php">
+                <input type="hidden" class="id" name="id">
+                <div class="form-group">
+                    <label for="edit_firstname" class="col-sm-3 control-label">Firstname</label>
+
+                    <div class="col-sm-9">
+                      <input type="text" class="form-control" id="edit_firstname" name="firstname">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="edit_lastname" class="col-sm-3 control-label">Lastname</label>
+
+                    <div class="col-sm-9">
+                      <input type="text" class="form-control" id="edit_lastname" name="lastname">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="edit_password" class="col-sm-3 control-label">Password</label>
+
+                    <div class="col-sm-9">
+                      <input type="password" class="form-control" id="edit_password" name="password">
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal"><i class="fa fa-close"></i> Close</button>
+              <button type="submit" class="btn btn-success btn-flat" name="edit"><i class="fa fa-check-square-o"></i> Update</button>
+              </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<!-- Delete -->
+<div class="modal fade" id="delete">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span></button>
+              <h4 class="modal-title"><b>Deleting...</b></h4>
+            </div>
+            <div class="modal-body">
+              <form class="form-horizontal" method="POST" action="voters_delete.php">
+                <input type="hidden" class="id" name="id">
+                <div class="text-center">
+                    <p>DELETE VOTER</p>
+                    <h2 class="bold fullname"></h2>
+                </div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal"><i class="fa fa-close"></i> Close</button>
+              <button type="submit" class="btn btn-danger btn-flat" name="delete"><i class="fa fa-trash"></i> Delete</button>
+              </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div id="send_email" class="modal fade">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4 class="modal-title">Send Email</h4>
+      </div>
+      <div class="modal-body">
+        <!-- Form untuk mengirim email -->
+        <form method="POST" action="kirim.php">
+          <table>
+            <tr>
+              <td>Email:</td>
+              <td><input type="email" name="email" class="form-control" required></td>
+            </tr>
+            <tr>
+              <td>Subjek:</td>
+              <td><input type="text" name="subjek" class="form-control" required></td>
+            </tr>
+            <tr>
+              <td>Pesan:</td>
+              <td><textarea name="pesan" cols="32" rows="5" class="form-control" required></textarea></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td><input type="submit" name="kirim" value="Kirim" class="btn btn-primary"></td>
+            </tr>
+          </table> 
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<!-- Update Photo -->
+<div class="modal fade" id="edit_photo">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span></button>
+              <h4 class="modal-title"><b><span class="fullname"></span></b></h4>
+            </div>
+            <div class="modal-body">
+              <form class="form-horizontal" method="POST" action="voters_photo.php" enctype="multipart/form-data">
+                <input type="hidden" class="id" name="id">
+                <div class="form-group">
+                    <label for="photo" class="col-sm-3 control-label">Photo</label>
+
+                    <div class="col-sm-9">
+                      <input type="file" id="photo" name="photo" required>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal"><i class="fa fa-close"></i> Close</button>
+              <button type="submit" class="btn btn-success btn-flat" name="upload"><i class="fa fa-check-square-o"></i> Update</button>
+              </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+     
